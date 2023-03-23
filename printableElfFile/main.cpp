7c2993a697020d79cf63a831cb943ac9525ac462
bladvak <tomek.n1313@gmail.com>
2023-03-23 16:47:19 +0100

    Create implementation of class PrintableEhdr32.

--- printableElfFile/main.cpp
+++ printableElfFile/main.cpp
@@ -4,20 +4,25 @@
 #include <fstream>
 #include <utility>
 using namespace std;
-#include "printableehdr64.h"
+#include "printableehdr32.h"
 
 
 int main(){
 
 fstream f("exe");
-void *ptr;
-if(f.is_open())
-    f.read(static_cast<char*>(ptr), sizeof(Elf64_Ehdr));
-PrintableEhdr64 e(ptr);
-auto a = e.machine();
-std::cout<<a.first<<" "<<a.second;
-
+void *ptr = new char[sizeof(Elf32_Ehdr)];
+if(f.is_open()){
+    f.read(static_cast<char*>(ptr), sizeof(Elf32_Ehdr));
+PrintableEhdr32 e(ptr);
+auto a = e.type();
+std::cout<<e.ident_class()<<"\n";
+std::cout<<e.ident_data()<<"\n";
+std::cout<<e.ident_version()<<"\n";
+std::cout<<a.first<<" "<<a.second<<"\n";
+std::cout<<e.shoff()<< " "<<e.phoff();
 f.close();
+}
+
 
     return 0;
 }
